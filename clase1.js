2d84c4bcae25819d2521033c90922206fbf372ff
Eduardo Reyes <eduarno.1@hotmail.com>
2020-12-29 22:46:47 -0600

    first commit

---
+++ clase1.js
@@ -0,0 +1,8 @@
+var nombre = "Eduardo",
+  apellido = "Reyes",
+  edad = 28;
+edad = "28 años";
+var peso = 75;
+
+console.log("Hola " + nombre + " " + apellido);
+console.log("Tengo " + edad + " años");

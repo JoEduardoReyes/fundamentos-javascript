a93c16dc4114c378aaa78c06bcfa3d22ead6c5dc
Eduardo Reyes <eduarno.1@hotmail.com>
2021-01-02 22:42:24 -0600

    convertimos las funciones de edad a arrows

--- clase-12.js
+++ clase-12.js
@@ -50,16 +50,15 @@ const MAYORIA_DE_EDAD = 18;
 const esMayorDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD;
 //esta funcion solo retorna un valor
 
-function imprimirSiEsMayorDeEdad(persona) {
-  if (esMayorDeEdad(persona)) {
-    console.log(`${persona.nombre} es mayor de edad`);
-  } else {
-    console.log(`${persona.nombre} es menor de edad`);
-  }
-}
+// function imprimirSiEsMayorDeEdad(persona) {
+//   if (esMayorDeEdad(persona)) {
+//     console.log(`${persona.nombre} es mayor de edad`);
+//   } else {
+//     console.log(`${persona.nombre} es menor de edad`);
+//   }
+// }
 
-function permitirAcceso(persona) {
-  if (!esMayorDeEdad(persona)) {
-    console.log("Acesso Denegado");
-  }
-}
+const imprimirSiEsMayorDeEdad = ({ nombre, edad }) =>
+  esMayorDeEdad({ edad })
+    ? console.log(`${nombre} es mayor de edad, puede pasar`)
+    : console.log(`${nombre} es menor de edad, acceso denegado`);

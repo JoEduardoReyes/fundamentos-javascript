cb2c82d54b46b03cbc9577f6d2f148f58725d46f
Eduardo Reyes <eduarno.1@hotmail.com>
2021-01-02 18:13:13 -0600

    Funciones

---
+++ clase4.js
@@ -0,0 +1,11 @@
+var nombre = "Eduardo",
+  edad = 22;
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad("vicky", 28);
+imprimirEdad("Eric", 32);
+imprimirEdad("Ana", 22);

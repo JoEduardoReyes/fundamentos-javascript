84a9b0b0e72d3d341a078176bbd747ff5feaaf38
Eduardo Reyes <eduarno.1@hotmail.com>
2021-01-06 22:04:24 -0600

    quitamos la variente persona y aun funciona

--- clase-17.js
+++ clase-17.js
@@ -37,6 +37,5 @@ var paula = {
 var personas = [sacha, alan, martin, dario, vicky, paula];
 
 for (var i = 0; i < personas.length; i++) {
-  var persona = personas[i];
-  console.log(`${persona.nombre} mide ${persona.altura}m`);
+  console.log(`${personas[i].nombre} mide ${personas[i].altura}m`);
 }

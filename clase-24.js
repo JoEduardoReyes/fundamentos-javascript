a66dce9d67372e7cba86d590fa0ac07e6eca3c06
Eduardo Reyes <eduarno.1@hotmail.com>
2021-01-10 22:19:04 -0600

    clases en JS

---
+++ clase-24.js
@@ -0,0 +1,30 @@
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.altura = altura;
+  }
+  saludar() {
+    console.log(
+      `Hola, me llamo ${this.nombre} ${this.apellido} y mido ${this.altura}m`
+    );
+  }
+  soyAlto() {
+    return this.altura > 1.8;
+  }
+}
+
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura) {
+    super(nombre, apellido, altura);
+  }
+  saludar() {
+    console.log(
+      `Hola me llamno ${this.nombre} ${this.apellido} y soy desarrollador/a`
+    );
+  }
+}
+
+// var eduardo = new Persona("Eduardo", "Reyes", 1.77);
+// var erika = new Persona("Erika", "Luna", 1.65);
+// var arturo = new Persona("Arturo", "Martines", 1.89);
